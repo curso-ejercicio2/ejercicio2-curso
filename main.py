3806e2f6ab0a44f9468ac9dd0398f6aec8ba1d03
DajhanaArceRocha <arcerochadajhana@gmail.com>
2026-09-09 22:15:55 -0400

    fix: agregar la funcion/Promedio

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def mostrar_menu():
     print("3. Multiplicación")
     print("4. Módulo")
     print("5. División")
+    print("7 Promedio")
     print("0. Salir")
     print("===============================")
 
@@ -49,7 +50,13 @@ def main():
                 a = float(input("Ingrese el dividendo: "))
                 b = float(input("Ingrese el divisor: "))
                 print(f"Resultado: {division.dividir(a, b)}")
-            
+
+            elif opcion == "7":
+                from operaciones import promedio
+                print("\n--- OPERACIÓN: PROMEDIO ---")
+                valores = list(map(float, input("Ingrese los valores separados por espacios: ").split()))
+                resultado = promedio.promedio(valores)
+                print("El promedio es:", resultado)
             else:
                 print("Opción no válida")
         

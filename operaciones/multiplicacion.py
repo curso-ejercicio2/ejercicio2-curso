fa4406538c2258c44d6d74d504fbdafcc465a0bc
darlin rosse <202401540est.umss.edu>
2026-09-09 20:24:26 -0400

    BugHunters Darlin: multiplicacion - agregar funcion para multiplicar matrices

--- operaciones/multiplicacion.py
+++ operaciones/multiplicacion.py
@@ -12,3 +12,17 @@ def multiplicar(a, b):
         float: Resultado de a * b
     """
     return a * b
+
+def multiplicar_matriz (matriz_a, matriz_b):
+    filas_a = len(matriz_a)
+    columnas_a = len(matriz_a[0])
+    columnas_b = len(matriz_b[0])
+
+    resultado = [[0 for _ in range(columnas_b)] for _ in range(filas_a)]
+
+    for i in range(filas_a):
+        for j in range(columnas_b):
+            for k in range(columnas_a):
+                resultado[i][j] += matriz_a[i][k] * matriz_b[k][j]
+
+    return resultado

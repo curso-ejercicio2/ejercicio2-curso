4c7deabce8a9a69ddc730c7e47ef406f6abe5ac2
A-Saavedra <alexcrisangel@gmail.com>
2026-09-11 11:01:00 -0400

    feat(eq07): implementa y agrega porcentaje al main

--- operaciones/porcentaje.py
+++ operaciones/porcentaje.py
@@ -3,14 +3,15 @@
 # Responsable: Alex Cristian Saavedra Veliz (eq07)
 
 def porcentaje(total: float, porcentaje: float) -> float:
-    """Calcula el porcentaje de una cantidad base.
+    """
+    Calcula el porcentaje de una cantidad base.
 
-    Ejemplo: porcentaje(200, 15) -> 30.0
+    Args:
+        total (float): Monto total
+        porcentaje (float): Porcentaje del total que se quiere calcular
+
+    Ejemplo: El 15% de 200
+    
+    porcentaje(200, 15) -> 30.0
     """
     return (total * porcentaje) / 100.0
-
-if __name__ == "__main__":
-    print("--- Pruebas Locales: Porcentaje ---")
-    base = 200.0
-    tasa = 15.0
-    print(f"{tasa}% de {base} = {porcentaje(base, tasa)}")

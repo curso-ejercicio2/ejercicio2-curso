fc855f26cd2b4c0781d872bd637fe37713d9272f
David5678g <david2023cabreraamargo@gmail.com>
2026-09-10 22:41:42 -0400

    Add porcentaje.py to calculate percentage

---
+++ operaciones/porcentaje.py
@@ -0,0 +1,7 @@
+# Responsable: David Cabrera (eq09)
+numero1 = float(input("Ingrese el primer numero: "))
+numero2 = float(input("Ingrese el segundo numero: "))
+
+porcentaje = (numero1 / numero2) * 100
+
+print(f"{numero1} es el {porcentaje}% de {numero2}")
